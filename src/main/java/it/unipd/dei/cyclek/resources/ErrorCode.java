871cddabbdb7589c7ae68c7fb0a4f3064609860e
Tomaz14 <marcotomaiuoli@gmail.com>
2024-04-25 21:50:01 +0200

    fix ErrorCode

--- src/main/java/it/unipd/dei/cyclek/resources/ErrorCode.java
+++ src/main/java/it/unipd/dei/cyclek/resources/ErrorCode.java
@@ -8,12 +8,12 @@ public enum ErrorCode {
     REGISTER_USER_BAD_REQUEST("-100", HttpServletResponse.SC_BAD_REQUEST, "Bad Request", "Missing some user's fields"),
     REGISTER_USER_INTERNAL_SERVER_ERROR("-101", HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Internal Server Error", "Error creating user"),
     REGISTER_USER_CONSTRAINT_VIOLATION("-102", HttpServletResponse.SC_BAD_REQUEST, "Bad Request", "Username already used"),
-    REGISTER_USER_DB_ERROR("-103", HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Internal Server Error", "Database Error"),
+    REGISTER_USER_DB_ERROR("-103", HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Internal Server Error", "Unexpected Error while registering user"),
     LOGIN_USER_BAD_REQUEST("-104", HttpServletResponse.SC_BAD_REQUEST, "Bad Request", "Missing username or password"),
-    LOGIN_USER_NOT_FOUND("-105", HttpServletResponse.SC_NOT_FOUND, "User Not Found", "User Not Found"),
-    LOGIN_USER_DB_ERROR("-106", HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Internal Server Error", "Database Error"),
-    UPDATE_USER_BAD_REQUEST("-107", HttpServletResponse.SC_BAD_REQUEST, "Bad Request", "Missing some user's fields"),
-    UPDATE_USER_DB_ERROR("-108", HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Internal Server Error", "Database Error"),
+    LOGIN_USER_NOT_FOUND("-105", HttpServletResponse.SC_NOT_FOUND, "Not Found", "User Not Found"),
+    LOGIN_USER_DB_ERROR("-106", HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Internal Server Error", "Unexpected Error while login user"),
+    UPDATE_USER_BAD_REQUEST("-107", HttpServletResponse.SC_BAD_REQUEST, "Bad Request", "Cannot update the user, body must contains all parameters"),
+    UPDATE_USER_DB_ERROR("-108", HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "Internal Server Error", "Unexpected Error while updating user"),
     // Stats Code       -200 -> -299
 
 
