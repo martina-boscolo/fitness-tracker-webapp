06c7cca96088492942352d7415e9276046b25e06
Tomaz14 <marcotomaiuoli@gmail.com>
2024-05-16 21:44:56 +0200

    add login logic

--- src/main/webapp/javascript/login.js
+++ src/main/webapp/javascript/login.js
@@ -1,32 +1,34 @@
-document.getElementById('loginForm').addEventListener('submit', function (event) {
+document.getElementById("loginForm").addEventListener("submit", function (event) {
     event.preventDefault(); // Prevent the default form submission
 
     // Get the form data
-    const username = document.getElementById('InputUsername').value;
-    const password = document.getElementById('InputPassword').value;
+    const username = document.getElementById("InputUsername").value;
+    const password = document.getElementById("InputPassword").value;
 
     const user = {
         username: username,
         password: password
     };
 
+    console.log(JSON.stringify(user))
     // Make the API call
-    fetch('http://localhost:8080/cycleK-1.0.0/rest/user/login', {
-        method: 'POST',
+    fetch("http://localhost:8080/cycleK_war_exploded/rest/user/login", {
+        method: "POST",
+        mode: "cors",
         headers: {
-            'Content-Type': 'application/json'
+            "Content-Type": "application/json"
         },
         body: JSON.stringify(user)
     })
         .then(response => response.json())
         .then(data => {
             // Handle the response data
-            console.log('Success:', data);
-            alert('Login successful!');
+            console.log("Success:", data);
+            alert("Login successful!");
             // Optionally, redirect the user or perform other actions
         })
         .catch(error => {
-            console.error('Error:', error);
-            alert('Login failed!');
+            console.error("Error:", error);
+            alert("Login failed!");
         });
 });
