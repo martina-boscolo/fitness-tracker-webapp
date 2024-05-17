f4b06bc6120f8b904ff743210cdf080fa3da654a
Tomaz14 <marcotomaiuoli@gmail.com>
2024-05-17 12:15:44 +0200

    add registration form

---
+++ src/main/webapp/javascript/register.js
@@ -0,0 +1,43 @@
+document.getElementById("RegisterForm").addEventListener("submit", function (event) {
+    event.preventDefault(); // Prevent the default form submission
+
+    // Get the form data
+    const name = document.getElementById("firstName").value;
+    const surname = document.getElementById("lastName").value;
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+    const date = document.getElementById("date").value;
+    const gender = document.getElementById("gender").value;
+
+    const user = {
+        name: name,
+        surname: surname,
+        username: username,
+        password: password,
+        date: date,
+        gender: gender
+    };
+
+    console.log(JSON.stringify(user))
+    // Make the API call
+    fetch("http://localhost:8080/cycleK_war_exploded/rest/user/signup", {
+        method: "POST",
+        mode: "cors",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(user)
+    })
+        .then(response => response.json())
+        .then(data => {
+            // Handle the response data
+            console.log("Success:", data);
+            alert("Login successful!");
+            // Optionally, redirect the user or perform other actions
+        })
+        .catch(error => {
+            console.error("Error:", error);
+            alert("Login failed!");
+        });
+});
+
