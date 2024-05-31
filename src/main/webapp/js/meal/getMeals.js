b1a431584e427d43817ace7dc69a38eabacd4d0f
RiccardoTrevisiol <riccardo.trevisiol.5@gmail.com>
2024-05-31 23:02:04 +0200

    developed js functions

--- src/main/webapp/js/meal/getMeals.js
+++ src/main/webapp/js/meal/getMeals.js
@@ -1,6 +1,16 @@
-fetch('http://localhost:8080/cycleK-1.0.0/rest/meal/user',{
-    credentials:'include'
-})
-.then(response => response.json())
-.then(data => {})
-.catch(error => console.error('Error: ', error));
+document.addEventListener('DOMContentLoaded', function () {
+    fetch('http://localhost:8080/cycleK-1.0.0/rest/meal/user', {
+        credentials: 'include'
+    })
+        .then(response => {
+            if(!response.ok)
+                throw new Error("Response from db was not ok");
+            console.log(response.json());
+            return response.json()
+        })
+        .then(data => {
+            console.log(data);
+
+        })
+        .catch(error => console.error('Error: ', error));
+});
