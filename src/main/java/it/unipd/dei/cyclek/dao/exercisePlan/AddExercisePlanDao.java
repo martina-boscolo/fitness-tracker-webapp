0f7cd6a5a6f1cddb7289e3dd30b5513004dac9d6
Kimia Abedini <kimia.abedini@studenti.unipd.it>
2024-06-03 17:55:21 +0200

    everything now works properly (need small changes in style)

--- src/main/java/it/unipd/dei/cyclek/dao/exercisePlan/AddExercisePlanDao.java
+++ src/main/java/it/unipd/dei/cyclek/dao/exercisePlan/AddExercisePlanDao.java
@@ -2,22 +2,28 @@ package it.unipd.dei.cyclek.dao.exercisePlan;
 
 import it.unipd.dei.cyclek.dao.AbstractDAO;
 import it.unipd.dei.cyclek.resources.entity.ExercisePlan;
+import it.unipd.dei.cyclek.resources.entity.UserStats;
 import org.postgresql.util.PGobject;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public class AddExercisePlanDao extends AbstractDAO<Boolean> {
+public class AddExercisePlanDao extends AbstractDAO<ExercisePlan> {
 
-    public static final String STATEMENT = "INSERT INTO exercise_plan (idUser, planName, plan) VALUES (?, ?, ?)";
+    public static final String STATEMENT = "INSERT INTO exercise_plan (idUser, planName, plan) VALUES (?, ?, ?) RETURNING *";
     private final ExercisePlan plan;
+
     public AddExercisePlanDao(Connection con, ExercisePlan plan) {
         super(con);
         this.plan = plan;
     }
+
     @Override
     protected final void doAccess() throws SQLException {
+        ResultSet rs;
+        ExercisePlan exerciseplan = null;
         try (PreparedStatement pstmt = con.prepareStatement(STATEMENT)) {
             pstmt.setInt(1, plan.getIdUser());
             pstmt.setString(2, plan.getPlanName());
@@ -27,9 +33,19 @@ public class AddExercisePlanDao extends AbstractDAO<Boolean> {
             jsonObject.setValue(plan.getPlan());
             LOGGER.debug("JSON String obtained from plan.getPlan(): {}", jsonObject);
             pstmt.setObject(3, jsonObject);
-            int rowsAffected = pstmt.executeUpdate();
-            // If rowsAffected > 0, exercisePlan was saved successfully
-            this.outputParam = rowsAffected > 0;
+            rs = pstmt.executeQuery();
+
+            if (rs.next()) {
+                exerciseplan = new ExercisePlan(
+                        rs.getInt("id"),
+                        rs.getInt("idUser"),
+                        rs.getString("planName"),
+                        rs.getString("plan"),
+                        rs.getString("planDate")
+                );
+                LOGGER.info("exercise plan added with id {}.", exerciseplan.getId());
+            }
+            this.outputParam = exerciseplan;
             LOGGER.info("ExercisePlan successfully saved.");
         }
     }
